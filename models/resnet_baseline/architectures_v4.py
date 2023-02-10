74d901a3ac2319eb36b1d7bb21d647c11a429d93
Moritz Wassmer <moritz@wassmer-bw.de>
2023-02-10 19:03:59 +0100

    added Resnet_Baseline_V4_No_Shuffle, not tested

--- models/resnet_baseline/architectures_v4.py
+++ models/resnet_baseline/architectures_v4.py
@@ -145,27 +145,6 @@ def mlp(neurons_in, neurons_out, neurons_hidden):
     ))
 
 
-def steer_head(neurons_in):  # [-1,1] Range Output
-    str_head = nn.Sequential(
-        nn.Linear(neurons_in, 1),
-        nn.Tanh())
-    return str_head
-
-
-def throttle_head(neurons_in):  # [0,1] Range Output
-    thr_head = nn.Sequential(
-        nn.Linear(neurons_in, 1),
-        nn.Sigmoid())
-    return thr_head
-
-
-def brake_head(neurons_in):  # [0,1] Range Output
-    brk_head = nn.Sequential(
-        nn.Linear(neurons_in, 1),
-        nn.Sigmoid())
-    return brk_head
-
-
 def to_cuda_if_possible(data, device): # TODO HAS TO BE LIKE THAT FOR INFERENCE
     return data.to(device) if device else data
 
@@ -183,7 +162,7 @@ class Resnet_Baseline_V4_Shuffle(nn.Module):
 
         # Top layer of ResNet which you can modify. We choose Identity to use it as Input for all the heads
         self.net.fc = nn.Sequential(
-            nn.Dropout(p=0, inplace=False)
+            nn.Dropout(p=0.5, inplace=False)
             # nn.Identity()
         )
 
@@ -284,5 +263,84 @@ class Resnet_Baseline_V4_Shuffle(nn.Module):
         brake = torch.unsqueeze(sorted_out[:, 1], 1)
         steer = torch.unsqueeze(sorted_out[:, 2], 1)
         throttle = torch.unsqueeze(sorted_out[:, 3], 1)
+
+        return brake, steer, throttle
+
+class Resnet_Baseline_V4_No_Shuffle(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+        # ResNet Architecture with pretrained weights, also bigger resnets available
+        self.net = torchvision.models.resnet34(weights=True)
+        num_ftrs = self.net.fc.in_features
+
+        # Top layer of ResNet which you can modify. We choose Identity to use it as Input for all the heads
+        self.net.fc = nn.Sequential(
+            nn.Dropout(p=0, inplace=False)
+            # nn.Identity()
+        )
+
+        self.spd_input = nn.Sequential(
+            nn.Linear(1, 128),
+            nn.Tanh(),
+            nn.Linear(128, 128),
+            nn.Tanh(),
+            nn.Linear(128, 128),
+            nn.Tanh()  # nn.LeakyReLU() # TODO
+            # nn.Dropout(p=0.5, inplace=False)
+        )
+
+        # shared MLP
+        self.shared = nn.Sequential(
+            nn.Identity()
+
+        )
+
+        self.branches_mlp = nn.ModuleList()
+        self.branches_brake = nn.ModuleList()
+        self.branches_steer = nn.ModuleList()
+        self.branches_throttle = nn.ModuleList()
+
+        # Create Branches for all Commands
+        for i in range(0, 7):
+            mlp_branch = mlp(num_ftrs + 128, 256, 256)
+            brk = brake_head(256)
+            thr = throttle_head(256)
+            steer = steer_head(256)
+
+            self.branches_mlp.append(mlp_branch)
+            self.branches_brake.append(brk)
+            self.branches_steer.append(steer)
+            self.branches_throttle.append(thr)
+
+            # Forward Pass of the Model
+
+    def forward(self, rgb, spd, batch_size):
+
+        # Pass until shared layer reached
+        rgb = self.net(rgb)  # BRG
+        spd = self.spd_input(spd)
+        x = torch.cat((rgb, spd), 1)
+        x = self.shared(x)
+
+        preds = []
+        for i in range(1,6):
+            mlp_output = self.branches_mlp[i](x)
+            brake = self.branches_brake[i](mlp_output[mapping_list[i]])
+            steer = self.branches_steer[i](mlp_output[mapping_list[i]])  #
+            throttle = self.branches_throttle[i](mlp_output[mapping_list[i]])
+            y_hat = torch.cat((brake, steer, throttle), axis=1)
+            preds.append(y_hat)
+
+
+        stacked = torch.cat(preds, 0)
+
+        # Extract Policy
+        brake = torch.unsqueeze(stacked[:, 1], 1)
+        steer = torch.unsqueeze(stacked[:, 2], 1)
+        throttle = torch.unsqueeze(stacked[:, 3], 1)
 
         return brake, steer, throttle
