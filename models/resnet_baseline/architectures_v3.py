c6499b79d33603a74c75c5823f91773654243341
JulianKlitzing <julian.von.klitzing@campus.tu-berlin.de>
2023-02-12 12:56:24 +0100

    Added dropout to Dropout Version of class

--- models/resnet_baseline/architectures_v3.py
+++ models/resnet_baseline/architectures_v3.py
@@ -11,74 +11,78 @@ def load_weights(net,name):
     return net
 
 
+
+
+
 class Resnet_Baseline_V3_Dropout(nn.Module):
 
     def __init__(self):
-        super().__init__()
+            super().__init__()
 
-        # ResNet Architecture with pretrained weights, also bigger resnets available
-        self.net = torchvision.models.resnet34(weights=True)
-        num_ftrs = self.net.fc.in_features
+            # ResNet Architecture with pretrained weights, also bigger resnets available
+            self.net = torchvision.models.resnet18(weights=True)
+            num_ftrs = self.net.fc.in_features
 
-        # Top layer of ResNet which you can modify. We choose Identity to use it as Input for all the heads
-        self.net.fc = nn.Sequential(
-            nn.Identity(),
-            # nn.Dropout(p=1, inplace=False)
-        )
+            # Top layer of ResNet which you can modify. We choose Identity to use it as Input for all the heads
+            self.net.fc = nn.Sequential(
+                nn.Identity(),
+                nn.Dropout(p=0.5, inplace=False)
+            )
 
-        # Input Layer fuer cmd, spd
-        self.cmd_input = nn.Sequential(
-            nn.Linear(7, 128),
-            nn.Tanh()
-            # nn.Dropout(p=0.5, inplace=False)
-        )
+            # Input Layer fuer cmd, spd
+            self.cmd_input = nn.Sequential(
+                nn.Linear(7, 128),
+                nn.Tanh(),
+                # nn.Dropout(p=0.5, inplace=False)
+            )
 
-        self.spd_input = nn.Sequential(
-            nn.Linear(1, 128),
-            nn.Tanh(),
-            # nn.Dropout(p=0.5, inplace=False)
-        )
+            self.spd_input = nn.Sequential(
+                nn.Linear(1, 128),
+                nn.Tanh(),
+                # nn.Dropout(p=0.5, inplace=False)
+            )
 
-        # MLP
-        self.mlp = nn.Sequential(
-            nn.Linear(num_ftrs + 128 + 128, num_ftrs + 128 + 128),
-            nn.Tanh(),  # nn.LeakyReLU()
-            nn.Dropout(p=0.5, inplace=False),
-            nn.Linear(num_ftrs + 128 + 128, num_ftrs + 128 + 128),
-            nn.Tanh(),  # nn.LeakyReLU()
-            nn.Dropout(p=0.5, inplace=False)
-        )
+            # MLP
+            self.mlp = nn.Sequential(
+                nn.Linear(num_ftrs + 128 + 128, num_ftrs + 128 + 128),
+                nn.Tanh(),  # nn.LeakyReLU()
+                # nn.Dropout(p=0.5, inplace=False),
+                nn.Linear(num_ftrs + 128 + 128, num_ftrs + 128 + 128),
+                nn.Tanh()  # , #nn.LeakyReLU()
+                # nn.Dropout(p=0.5, inplace=False)
+            )
 
-        # Regression Heads for Throttle, Brake and Steering
-        self.thr_head = nn.Sequential(
-            nn.Linear(num_ftrs + 128 + 128, 1),
-            nn.Sigmoid()  # [0,1] Range Output
+            # Regression Heads for Throttle, Brake and Steering
+            self.thr_head = nn.Sequential(
+                nn.Linear(num_ftrs + 128 + 128, 1),
+                nn.Sigmoid()  # [0,1] Range Output
 
-        )
+            )
 
-        self.brk_head = nn.Sequential(
-            nn.Linear(num_ftrs + 128 + 128, 1),
-            nn.Sigmoid()  # [0,1] Range Output
+            self.brk_head = nn.Sequential(
+                nn.Linear(num_ftrs + 128 + 128, 1),
+                nn.Sigmoid()  # [0,1] Range Output
 
-        )
+            )
 
-        self.str_head = nn.Sequential(
-            nn.Linear(num_ftrs + 128 + 128, 1),
-            nn.Tanh()  # [-1,1] Range Output
+            self.str_head = nn.Sequential(
+                nn.Linear(num_ftrs + 128 + 128, 1),
+                nn.Tanh()  # [-1,1] Range Output
 
-        )
+            )
 
     # Forward Pass of the Model
     def forward(self, rgb, cmd, spd):
         rgb = self.net(rgb)  # BRG
-        # rgb = self.net.fc(rgb)
         cmd = self.cmd_input(cmd)
         spd = self.spd_input(spd)
 
         x = torch.cat((rgb, cmd, spd), 1)
         x = self.mlp(x)
 
-        return self.brk_head(x), self.str_head(x), self.thr_head(x)
+        # x = self.net.fc(x)
+        return self.brk_head(x), self.str_head(x), self.thr_head(x)  # TODO 3 Change new order
+        # return self.thr_head(x), self.str_head(x), self.brk_head(x)
 
 class Resnet_Baseline_V3(nn.Module):
 
