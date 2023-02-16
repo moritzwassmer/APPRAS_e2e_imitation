3474d13b0c8d25564b42cb6fec41bb1d9455b3ff
JulianKlitzing <julian.von.klitzing@campus.tu-berlin.de>
2023-02-16 22:24:21 +0100

    Create dataset_final.py

---
+++ data_pipeline/dataset_final.py
@@ -0,0 +1,102 @@
+from torch.utils.data import Dataset
+import datetime
+from torchvision import transforms
+import torch
+import pandas as pd
+import numpy as np
+import os
+import cv2
+import json
+"""
+Settings:
+
+1.
+x: rgb, command, speed
+y: brake, steer, throttle
+
+2. 
+x: rgb, lidar, command, speed
+y: brake, steer, throttle
+
+3.
+x: rgb, command, speed
+y: waypoints
+
+4. 
+x: rgb, lidar, command, speed
+y: waypoints
+
+5.
+x: rgb, x, y, speed
+y: waypoints
+
+6.
+x: rgb, lidar, x, y, speed
+y: waypoints
+"""
+
+class CARLADatasetXYOpt(Dataset):
+
+    def __init__(self, df_meta_data):
+        """
+        Args:
+            root_dir (string): Directory with all the images.
+            used_inputs (list): Contains the folder names of the inputs that shall be used.
+            used_measurements (list): Contains the attributes of the measurements that shall be used.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        df_paths_rgb = df_meta_data["dir"] + os.sep + "rgb" + os.sep + df_meta_data["rgb"]
+        df_paths_measurements = df_meta_data["dir"] + os.sep + "measurements" + os.sep + df_meta_data["measurements"]
+        self.rgb_paths = df_paths_rgb.to_numpy()
+        self.measurements_paths = df_paths_measurements.to_numpy()
+        #  hotfix for model trainer
+        # self.y = np.array(["brake", "steer", "throttle"])
+        self.y = np.array(["waypoints"])
+
+    def __len__(self):
+        return len(self.rgb_paths)
+
+
+    def __getitem__(self, idx):
+
+        rgb_np = self.load_rgb(self.rgb_paths[idx])
+        measurements = self.load_measurements(self.measurements_paths[idx])
+        speed = measurements["speed"]
+        command = measurements["command"]
+
+        waypoints_global = np.array(measurements["waypoints"])[:4]
+        compass = measurements["theta"]
+        x_position = measurements["x"]
+        y_position = measurements["y"]
+        waypoints_local = self.project_global_waypoints_to_local(waypoints_global, x_position, y_position, compass)
+
+        x_sample = {"rgb": rgb_np, "command": command, "speed": np.array([speed])}
+        y_sample = {"waypoints": waypoints_local}
+        return x_sample, y_sample, idx
+
+    def load_rgb(self, path):
+        img = cv2.imread(path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # TODO CHANGED TO RGB
+        # Reshape to C,H,W
+        img = np.transpose(img, (2, 0, 1))
+        img = img.astype("float32")
+        return img
+
+    def load_measurements(self, path):
+        with open(path, 'r') as f:
+            measurements = json.load(f)
+        return measurements
+    
+    def project_global_waypoints_to_local(self, waypoints, x, y, compass):
+        theta = compass + np.pi/2
+        waypoints = np.asarray(waypoints)
+        waypoints = waypoints[:, :2]
+        R = np.array([
+                [np.cos(theta), -np.sin(theta)],
+                [np.sin(theta), np.cos(theta)]
+                ])
+
+        local_command_points = np.array([waypoints[:,0]-x, waypoints[:,1]-y])
+        local_command_points = R.T.dot(local_command_points)
+        return local_command_points.T
