c8e46ead17de56f7af404acb9b4db648c348c0c1
Moritz Wassmer <moritz@wassmer-bw.de>
2023-02-27 21:18:47 +0100

    premerge quicksave

--- models/resnet_lidar/lidar_v1.py
+++ models/resnet_lidar/lidar_v1.py
@@ -118,29 +118,29 @@ class Resnet_Lidar_V1_Dropout(nn.Module):
 
             # MLP
             self.mlp = nn.Sequential(
-                nn.Linear(num_ftrs_rgb + num_ftrs_lidar + 128 + 128, num_ftrs_rgb + num_ftrs_lidar + 128 + 128),
+                nn.Linear(num_ftrs_rgb + num_ftrs_lidar + 128 + 128, num_ftrs_rgb + 128 + 128),
                 nn.ReLU(),
                 nn.Dropout(p=dropout_rate, inplace=False),
-                nn.Linear(num_ftrs_rgb + num_ftrs_lidar + 128 + 128, num_ftrs_rgb + num_ftrs_lidar + 128 + 128),
+                nn.Linear(num_ftrs_rgb +  128 + 128, num_ftrs_rgb + 128 + 128),
                 nn.ReLU(), 
                 nn.Dropout(p=dropout_rate, inplace=False)
             )
 
             # Regression Heads for Throttle, Brake and Steering
             self.thr_head = nn.Sequential(
-                nn.Linear(num_ftrs_rgb + num_ftrs_lidar + 128 + 128, 1),
+                nn.Linear(num_ftrs_rgb  + 128 + 128, 1),
                 nn.Sigmoid()  # [0,1] Range Output
 
             )
 
             self.brk_head = nn.Sequential(
-                nn.Linear(num_ftrs_rgb + num_ftrs_lidar + 128 + 128, 1),
+                nn.Linear(num_ftrs_rgb + 128 + 128, 1),
                 nn.Sigmoid()  # [0,1] Range Output
 
             )
 
             self.str_head = nn.Sequential(
-                nn.Linear(num_ftrs_rgb + num_ftrs_lidar + 128 + 128, 1),
+                nn.Linear(num_ftrs_rgb + 128 + 128, 1),
                 nn.Tanh()  # [-1,1] Range Output
 
             )
