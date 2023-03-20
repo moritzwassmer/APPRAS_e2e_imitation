e937f3ae553729814886564522e6f06621f73591
Jakob Wiren <jakowiren@gmail.com>
2023-03-20 17:16:44 +0100

    Documentation architectures v1-v3

--- models/resnet_rgb/architectures_v1.py
+++ models/resnet_rgb/architectures_v1.py
@@ -2,16 +2,26 @@ import torch
 import torch.nn as nn
 import torchvision
 
-"""
-Input: rgb, speed, navigational command.
-Output: break, steer, throttle.
-Seq len: 1
-Fusion: late fusion (concatenating).
-Comment: no dense layers after concatenation. 
-"""
 
 class Resnet_Baseline_V1(nn.Module):
     
+
+    """ 
+    Class implementing the ResNet18 architecture with added input
+    layers and regression heads.
+
+    This model serves as a first attempt to use the ResNet architecture for our experiment.
+
+    Attributes:
+        net: vision backbone (ResNet)
+        cmd_input: command input layer - one hot encoded vector with 7 different navigational commands
+        spd_input: speed input layer
+        thr_head: Regression Heads for Throttle
+        brk_head: Regression Heads for Brake
+        str_head: Regression Heads for Steering
+    """
+
+
     def __init__(self):
         super().__init__()
         # ResNet Architecture with pretrained weights, also bigger resnets available
@@ -21,15 +31,15 @@ class Resnet_Baseline_V1(nn.Module):
         # Top layer of ResNet which you can modify. We choose Identity to use it as Input for all the heads
         self.net.fc = nn.Identity()
         
-        # Input Layer fuer cmd, spd
+        # Input Layer for cmd, spd
         self.cmd_input = nn.Sequential(
             nn.Linear(7, 7),
-            nn.LeakyReLU() # TODO
+            nn.LeakyReLU() 
         )
         
         self.spd_input = nn.Sequential(
             nn.Linear(1, 1),
-            nn.LeakyReLU() # TODO
+            nn.LeakyReLU() 
         )
         
         # Regression Heads for Throttle, Brake and Steering
@@ -53,9 +63,7 @@ class Resnet_Baseline_V1(nn.Module):
         rgb = self.net(rgb) # BRG
         cmd = self.cmd_input(cmd)
         spd = self.spd_input(spd)
-        # print(rgb.shape, cmd.shape, spd.shape)
         
         x = torch.cat((rgb, cmd, spd),1)
         
-        #return self.thr_head(x), self.str_head(x), self.brk_head(x) 
         return self.brk_head(x), self.str_head(x), self.thr_head(x) # Sorting!
